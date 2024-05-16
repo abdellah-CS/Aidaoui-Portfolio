26bf1b3af8fdd62025e25b76bce4aa1ee0c038e3
abdellah-CS <aidaoui.abdellah.05@gmail.com>
2024-05-16 17:09:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Define entry points for your PDF and video files
+        main: './src/main.tsx', // Replace with your actual entry point
+        pdf: './src/assets/cv.pdf',
+        video: './src/assets/bg-video.mp4',
+      },
+      // Ensure that Vite treats PDF and video files as assets
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 });
