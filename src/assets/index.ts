078656106a3d8e99c96ee67d10ee4d4781f74afd
abdellah-CS <aidaoui.abdellah.05@gmail.com>
2024-05-16 15:58:07 +0100

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,95 @@
+
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import wordpress from "./tech/wordpress.png";
+
+import khadmoney from "./khadmoney-logo.png";
+
+import apextech from "./APEXTECH.png";
+import apextech1 from "./apextech1.png";
+import apextech2 from "./apextech2.png";
+import apextech3 from "./apextech3.png";
+import apextech4 from "./apextech4.png";
+import apextech5 from "./apextech5.png";
+import homework from "./khadmoney.jpg";
+import khadmoneyacademy from "./khadmoneyacademy.jpg";
+import portfolio from "./portfolio.jpg";
+import world from "./world.png";
+import khadmoney1 from "./khadmoney1.png";
+import khadmoney2 from "./khadmoney2.png";
+import khadmoney3 from "./khadmoney3.png";
+import khadmoney4 from "./khadmoney4.png";
+import khadmoney5 from "./khadmoney5.png";
+import homework1 from "./homework1.png";
+import homework2 from "./homework2.png";
+import homework3 from "./homework3.png";
+import homework4 from "./homework4.png";
+import homework5 from "./homework5.png";
+import homework6 from "./homework6.png";
+import homework7 from "./homework7.png";
+
+export {
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  world,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  wordpress,
+  khadmoney,
+  apextech,
+  apextech1,
+  apextech2,
+  apextech3,
+  apextech4,
+  apextech5,
+  khadmoney1,
+  khadmoney2,
+  khadmoney3,
+  khadmoney4,
+  khadmoney5,
+  homework,
+  homework1,
+  homework2,
+  homework3,
+  homework4,
+  homework5,
+  homework6,
+  homework7,
+  khadmoneyacademy,
+  portfolio
+};
