67cc27f4298c1e7d51b889e50988d240085393ea
abdellah-CS <aidaoui.abdellah.05@gmail.com>
2024-05-16 17:41:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],// Specify which files to treat as static assets
+  plugins: [react()],
+  build: {
+    // Exclude the video file from Rollup bundling
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx',
+      },
+    },
+  },
 });
