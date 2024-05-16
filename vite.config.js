8ff5718e6d4354ed64ca6a7a7d5a7e92c88d77c5
abdellah-CS <aidaoui.abdellah.05@gmail.com>
2024-05-16 17:37:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Set the base URL for assets
-  assetsInclude: ['**/*.pdf', '**/*.mp4'], // Specify which files to treat as static assets
+  plugins: [react()],// Specify which files to treat as static assets
 });
