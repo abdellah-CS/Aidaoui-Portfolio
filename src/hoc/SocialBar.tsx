a319166d7f685c32fd6cbd4234e45bb26ab15c00
abdellah-CS <aidaoui.abdellah.05@gmail.com>
2024-05-17 15:17:20 +0100

    Update SocialBar.tsx

--- src/hoc/SocialBar.tsx
+++ src/hoc/SocialBar.tsx
@@ -16,7 +16,7 @@ const SocialBar: React.FC = () => {
                 <a className='flex justify-around items-center w-full rounded-lg h-14 bg-gray-900 font-semibold ml-[-110px] hover:ml-[0px] duration-500' href="https://github.com/abdellah-CS" target="_blank">Github <GitHubIcon fontSize="large"/></a>
             </li>
             <li className='flex w-44 h-16 justify-between items-center rounded-lg'>
-                <a className='flex justify-around items-center w-full rounded-lg h-14 bg-red-900 font-semibold ml-[-110px] hover:ml-[0px] duration-500' href="#contact">Gmail <EmailIcon fontSize="large"/></a>
+                <a className='flex justify-around items-center w-full rounded-lg h-14 bg-red-900 font-semibold ml-[-110px] hover:ml-[0px] duration-500' href="contact">Gmail <EmailIcon fontSize="large"/></a>
             </li>
             <li className='flex w-44 h-16 justify-between items-center rounded-lg'>
                 <a className='flex justify-around items-center w-full rounded-lg h-14 bg-cyan-500 font-semibold ml-[-110px] hover:ml-[0px] duration-500' href="https://t.me/AbdellahCS" target="_blank">Telegram <TelegramIcon fontSize="large"/></a>
